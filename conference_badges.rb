af23cc287baab02ac75258a7780e5dd57794acb0
Shuo Wang <gken90@gmail.com>
2018-08-14 22:39:54 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,5 +1,5 @@
-#badge_maker("Arel")
-#=> "Hello, my name is Arel."
+badge_maker("Arel")
+=> "Hello, my name is Arel."
 
 def badge_maker(name)
   return "Hello, my name is #{name}."
@@ -17,13 +17,24 @@ def batch_badge_creator(array)
   return new_array
 end
 
+# def assign_rooms(array)
+#   new_array = []
+#   array.each_with_index {|name, index|
+#     new_array << "Hello, #{name}! You'll be assigned to room #{index + 1}!"
+#   }
+#   return new_array
+# end
+
 def assign_rooms(array)
   new_array = []
   array.each_with_index {|name, index|
-    new_array << "Hello, #{name}! You'll be assigned to room #{index + 1}!"
-  }
-  return new_array
+  index += 1
+  string = "Hello, #{array}! You'll be assigned to room #{index}!"
+  new_array << string}
 end
+return new_array
+end
+
 
 def printer(attendees)
   batch_badge_creator(attendees).each do |badge|
@@ -33,3 +44,5 @@ def printer(attendees)
     puts room_assignment
   end
 end
+
+#++++++++++++++++++++++++++++++++++++++++++
